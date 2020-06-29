66d97b333972426f9613a6496ea852253e19db57
guilherme munis <gmmunis@hotmail.com>
2020-06-29 19:59:48 -0300

    first commit - API ciclo de pagamentos

---
+++ backend/config/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+module.exports = function(server) {
+
+  // API Routes
+  const router = express.Router();
+  server.use('/api', router);
+
+  // rotas da API
+  const billingCycleService = require('../api/billingCycle/billingCycleService');
+  billingCycleService.register(router, '/billingCycles');
+
+  const billingSumaryService = require('../api/billingSumary/billingSumaryService');
+  router.route('/billingSumary').get(billingSumaryService.getSummary);
+}
