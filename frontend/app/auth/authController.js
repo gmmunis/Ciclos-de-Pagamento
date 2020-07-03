1ee886b42b3e189d2b6bef385e5326c02f73fb3a
guilherme munis <gmmunis@hotmail.com>
2020-07-03 18:51:01 -0300

    autentificação do frontend - widget do usuário e estrutura básica do login e cadastro

---
+++ frontend/app/auth/authController.js
@@ -0,0 +1,29 @@
+(function() {
+  angular.module('primeiraApp').controller('AuthCtrl', [
+    '$location',
+    'msgs',
+    AuthController
+  ])
+  
+  function AuthController($location, msgs) {
+    const vm = this
+
+    vm.loginMode = true
+
+    vm.changeMode = () => vm.loginMode = !vm.loginMode
+
+    vm.login = () => {
+      console.log(`Login...${vm.user.email}`)
+    }
+
+    vm.signup = () => {
+      console.log(`Signup... ${vm.user.email}`)
+    }
+  
+    vm.getUser = () => ({name: 'Usuário MOCK', email: 'mock@hotmail.com.br'})
+  
+    vm.logout = () => {
+      console.log('Logout...')
+    }
+  }
+})()
