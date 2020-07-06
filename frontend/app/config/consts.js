e47d09a36340e0bd0894a97a7b515d56e0a34acc
guilherme munis <gmmunis@hotmail.com>
2020-07-06 15:31:37 -0300

    autentificação do frontend - auth-factory, proteção das rotas, validação do token e tratamento de respostas de erro

--- frontend/app/config/consts.js
+++ frontend/app/config/consts.js
@@ -5,6 +5,8 @@ angular.module('primeiraApp').constant('consts', {
   year: '2020',
   site: 'https://github.com/gmmunis/Ciclos-de-Pagamento',
   apiUrl: 'http://localhost:3003/api',
+  oapiUrl: 'http://localhost:3003/oapi',
+  userKey: '_primeira_app_user'
 }).run(['$rootScope', 'consts', function ($rootScope, consts) {
   $rootScope.consts = consts
 }])
